d07fadb6b385761127302386ad08da51779ae102
Amr Abd-Alkrim <wizmoori@gmail.com>
2021-04-08 17:10:20 +0200

    add manifest and init files

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'FireBits Resources Creation',
+    'version': '13.0.0.1',
+    'summary': 'Limit creation access for partners and products to specific users',
+    # 'description': '''
+    # ''',
+    'category': 'Resources',
+    'author': 'FireBits',
+    'sequence':-52,
+    'website': 'https://firebits.net',
+    # 'license': '',
+    'depends': [
+        'base',
+        'product'
+    ],
+    'data': [
+        'views/view.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
