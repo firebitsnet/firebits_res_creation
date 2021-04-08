fe908a3b8f0fee7f5b97a8c56c9011e997ece461
Amr Abd-Alkrim <wizmoori@gmail.com>
2021-04-08 17:30:52 +0200

    add res mixin unlink method

--- models/models.py
+++ models/models.py
@@ -15,3 +15,12 @@ class ResourcesAccessMixin(models.AbstractModel):
                 raise AccessError(hq_warnning)
 
         return super().create(vals)
+
+    def unlink(self):
+
+        if self.env.user:
+            if not self.env.user.has_group('firebits_res_creation.group_create_res'):
+                raise AccessError(hq_warnning)
+
+        return super().unlink()
+
