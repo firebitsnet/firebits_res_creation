356c1893b472463d0e9848daa74560220cf33d96
Amr Abd-Alkrim <wizmoori@gmail.com>
2021-04-08 17:30:36 +0200

    add res mixin create method

--- models/models.py
+++ models/models.py
@@ -4,3 +4,14 @@ hq_warnning = _("""
 The HQ or your manager has stoped the resources creation at this moment. 
 Please contact them if you think this is a problem.
 """)
+
+class ResourcesAccessMixin(models.AbstractModel):
+    _name = "res.firebits.mixin"
+
+    def create(self, vals):
+
+        if self.env.user:
+            if not self.env.user.has_group('firebits_res_creation.group_create_res'):
+                raise AccessError(hq_warnning)
+
+        return super().create(vals)
