e57ae48e94275b12c0fe816c0e58bdfc03b6511b
Amr Abd-Alkrim <wizmoori@gmail.com>
2022-07-23 17:12:14 +0200

    update

--- __manifest__.py
+++ __manifest__.py
@@ -1,5 +1,11 @@
 {
-    'name': 'FireBits Resources Creation',
+    'name': 'Limit resources creation access',
+    'images': [
+        'static/description/screenshot1.png',
+        'static/description/screenshot2.png',
+        'static/description/screenshot3.png',
+        'static/description/main_screenshot.png',
+    ],
     'version': '13.0.0.1',
     'summary': 'Limit creation access for partners and products to specific users',
     'description': '''
@@ -13,7 +19,7 @@
     'support': 'support@firebits.net',
     'sequence':-52,
     'website': 'https://firebits.net',
-    'live_test_url': 'https://www.youtube.com/watch?v=Qu6R3yNKR60',
+    'live_test_url': 'https://www.youtube.com/firebits',
     'demo': [],
     'depends': [
         'base',
